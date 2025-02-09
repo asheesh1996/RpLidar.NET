c04072a0cf4d1c279119ee6c240103343055121a
asheesh.maheshwari <asheesh.maheshwari@siemens.com>
2025-02-09 22:09:25 +0530

    WIP : Testes and Working on A1M8

---
+++ RpLidar.NET.Example/Program.cs
@@ -0,0 +1,36 @@
+﻿using RpLidar.NET.Entities;
+
+namespace RpLidar.NET.Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string PORT_NAME = "/dev/ttyUSB0";
+
+            Console.WriteLine("Starting RpLidar Test");
+            RPLidar lidar = new RPLidar(PORT_NAME);
+            lidar.LidarPointScanEvent += Lidar_LidarPointScanEvent;
+            Console.WriteLine("Lidar connected");
+            bool isRunning = true;
+            Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) =>
+            {
+                isRunning = false;
+            };
+            while (isRunning)
+            {
+                Thread.Sleep(10);
+            }
+            lidar.Dispose();
+        }
+
+        private static void Lidar_LidarPointScanEvent(List<LidarPoint> points)
+        {
+            foreach (LidarPoint point in points)
+            {
+                Console.WriteLine(point.ToString());
+            }
+        }
+    }
+}
+  
